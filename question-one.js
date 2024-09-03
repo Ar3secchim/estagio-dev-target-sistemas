a6d497e1d4b3a886ef9cc850d943e9117d98c4be
Renara Secchim <renarasecchim@gmail.com>
2024-09-03 09:54:30 -0300

    refactor: :recycle: add documentation function

--- question-one.js
+++ question-one.js
@@ -1,13 +1,21 @@
 // 1) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 
+/**
+ * Verifica se um número pertence à sequência de Fibonacci.
+ *
+ * @param {number} numero - O número a ser verificado.
+ * @returns {string} Uma mensagem indicando se o número pertence ou não à sequência de Fibonacci.
+ */
 function verificaFibonacci(numero) {
   let a = 0;
   let b = 1;
 
+  // Verifica casos especiais: 0 e 1
   if (numero === 0 || numero === 1) {
     return `${numero} pertence à sequência de Fibonacci.`;
   }
 
+  // Gera a sequência de Fibonacci até encontrar ou ultrapassar o número
   while (b <= numero) {
     if (b === numero) {
       return `${numero} pertence à sequência de Fibonacci.`;
@@ -17,6 +25,7 @@ function verificaFibonacci(numero) {
     a = temp;
   }
 
+  // Se o loop terminar sem encontrar o número, ele não pertence à sequência
   return `${numero} não pertence à sequência de Fibonacci.`;
 }
 
@@ -31,14 +40,6 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-/**
- * Esta função utiliza o método 'question' do objeto 'readline' para exibir uma mensagem
- * ao usuário e aguardar sua entrada. Após receber o número, ela:
- * 1. Converte a entrada do usuário para um número inteiro usando parseInt().
- * 2. Chama a função verificaFibonacci() com o número convertido.
- * 3. Exibe o resultado no console.
- * 4. Fecha a interface readline para encerrar o programa.
- */
 readline.question(
   "Digite um número para verificar se pertence à sequência de Fibonacci: ",
   (numeroInformado) => {
